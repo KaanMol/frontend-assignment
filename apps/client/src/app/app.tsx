0d78148e5c5e9b21b3237810f39d63f09550de1d
Kaan <kaanmol@outlook.com>
2023-03-12 23:48:32 +0100

    Builded simple dashboard

--- apps/client/src/app/app.tsx
+++ apps/client/src/app/app.tsx
@@ -1,24 +1,83 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { LineChart, Map, TextStatistics } from '@front-end-assignment/ui';
+import { Page } from '@front-end-assignment/ui';
+import { t } from 'i18next';
 import { useEffect, useState } from 'react';
-import useWebSocket from 'react-use-websocket';
-import styles from './app.module.css';
+import { useTranslation } from 'react-i18next';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+import i18next from '../translations';
 
-import NxWelcome from './nx-welcome';
+type BusData = {
+	time: Date,
+	energy: number,
+	gps: [number, number],
+	odo: number,
+	speed: number,
+	soc: number
+}
 
 export function App() {
-	const { sendMessage, lastMessage, readyState } = useWebSocket("ws://localhost:3000");
+	const { t } = useTranslation();
+	const [state, setState] = useState([] as BusData[]);
+	const { lastMessage, readyState } = useWebSocket("ws://localhost:3000");
 
 	useEffect(() => {
-		console.log(lastMessage)
+		const data = lastMessage?.data;
+		if (data === undefined) return;
+
+		let parsedData = JSON.parse(data);
+		parsedData.gps = parsedData.gps.split("|");
+		parsedData.time = new Date(parsedData.time);
+
+		if (state.length >= 500) {
+			state.shift();
+		}
+
+		setState([...state, parsedData])
 	}, [lastMessage]);
 
 	return (
-		<>
-
-
-			<div />
-		</>
+		<Page>
+			{
+				state.at(-1) === undefined || readyState !== ReadyState.OPEN ?
+					<p>Loading</p> :
+					<Dashboard state={state} />
+			}
+		</Page>
 	);
+}
+
+function Dashboard({ state }) {
+	const { t } = useTranslation();
+	const lastMessage = state.at(-1);
+	const textStatisticsData = [
+		{
+			title: t("dashboard.time"),
+			value: lastMessage.time.toLocaleTimeString()
+		},
+		{
+			title: t("dashboard.odometer"),
+			value: `${lastMessage.odo} km`
+		},
+		{
+			title: t("dashboard.energy"),
+			value: `${lastMessage.energy} kW`
+		},
+		{
+			title: t("dashboard.speed"),
+			value: `${lastMessage.speed} km/h`
+		},
+		{
+			title: t("dashboard.charge"),
+			value: `${lastMessage.soc} %`
+		}
+	]
+	return <>
+		<Map title="Live position" position={lastMessage.gps} />
+		<TextStatistics title="Current statistics" statistics={textStatisticsData} />
+		<LineChart title="Speed over time" data={state} dataKey="speed" />
+		<LineChart title="State of charge over time" data={state} dataKey="soc" />
+	</>
 }
 
 export default App;
