49b94a3fb63c766e73286cf359c60238224b8fe7
Kaan <kaanmol@outlook.com>
2023-03-12 23:47:40 +0100

    Added translations

---
+++ apps/client/src/translations/index.ts
@@ -0,0 +1,21 @@
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import i18n from "i18next";
+import en from './en.json';
+import nl from './nl.json';
+
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		resources: {
+			en,
+			nl
+		},
+		fallbackLng: "en",
+		interpolation: {
+			escapeValue: false, // not needed for react as it escapes by default
+		}
+	});
+
+export default i18n;
